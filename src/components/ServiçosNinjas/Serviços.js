33ea9f58d085b0d869863e5053267d94d59bf3a5
Karen Gaspar <karenasgaspar@gmail.com>
2021-11-24 15:47:14 -0300

    Adicionei função de trocar tela no app, adiconei início de código de filtros no componente serviços e logo no Header

--- src/components/ServiçosNinjas/Serviços.js
+++ src/components/ServiçosNinjas/Serviços.js
@@ -2,15 +2,55 @@ import React, { Component } from 'react'
 import { CardServiços } from './CardServiços'
 
 export class Serviços extends Component {
+  state = {
+    valorMinimo: "",
+    valorMaximo: "",
+    buscaProduto: "",
+    ordem: ""
+  }
+
+  atualizaValorMinimo = (event) => {
+    this.setState({ valorMinimo: event.target.value })
+  }
+  atualizaValorMaximo = (event) => {
+    this.setState({ valorMaximo: event.target.value })
+  }
+  atualizaBusca = (event) => {
+    this.setState({ buscaProduto: event.target.value })
+
+  }
+  atualizaOrdem = (event) => {
+    this.setState({ ordem: event.target.value })
+  }
+
+
   render() {
     return (
+
       <div>
-        <h3>Tela de lista de serviços</h3>
-        <input/>
-        <input/>
-        <input/>
-        <select/>
-        <CardServiços/>
+        <input
+          placeholder='Valor Min'
+        // value={}
+        // onChange={}
+        />
+        <input
+          placeholder='Valor Max'
+        // value={}
+        // onChange={}
+        />
+        <input
+          placeholder='Busca por serviço ou descrição'
+        // value={}
+        // onChange={}
+        />
+        <select>
+          <option> Sem ordenação </option>
+          <option> Valor Mínimo </option>
+          <option> Valor Máximo </option>
+          <option> Título </option>
+          <option> Prazo </option>
+        </select>
+        <CardServiços />
       </div>
     )
   }
