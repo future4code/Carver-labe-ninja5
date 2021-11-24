d7540cce686dcd8df07809451b70dab7a83defe7
desatinar <rodrigosdn@gmail.com>
2021-11-24 20:42:13 -0300

    adicionado imagens de footer com links direcionados

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import logoTwitter from '../img/logoTwitter.png'
+import logoFacebook from '../img/logoFacebook.png'
+import logoInstagram from '../img/logoInstagram.png'
+
+
 
 const EstiloFooter = styled.div`
 
 background-color: #a873e8;
-height: 70px;
+height: 89px;
 position: relative;
 bottom: 0;
+display: flex;
+align-items: center;
+justify-content: center;
+
+
+img{
+  height: 65px;
+}
 
 
 `
@@ -15,7 +28,9 @@ export class Footer extends Component {
   render() {
     return (
       <EstiloFooter>
-        
+        <a href='https://www.facebook.com' target="_blank"><img src={logoFacebook} alt='Logo do Facebook'/></a>
+        <a href='https://www.twitter.com' target="_blank"><img src={logoTwitter} alt='Logo do Twitter'/></a>
+        <a href='https://www.instagram.com' target="_blank"><img src={logoInstagram} alt='Logo do Instagram' /></a>
       </EstiloFooter>
     )
   }
