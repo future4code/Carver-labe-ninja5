7012978f55b31c3b844b305628a1ddcde12384de
desatinar <rodrigosdn@gmail.com>
2021-11-24 14:42:24 -0300

    alteracoes do body

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,28 +3,6 @@ import styled from 'styled-components'
 import logoLabeninjas from './imagemLogo/labeninjas.png'
 
 
-const Botao = styled.button`
-
-  background-color: #a873e8;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  -webkit-transition-duration: 0.4s; /* Safari */
-  transition-duration: 0.4s;
-
-:hover {
-
-  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-}
-
-`
-
 const DivBody = styled.div`
 
 background-color: #eef491;
