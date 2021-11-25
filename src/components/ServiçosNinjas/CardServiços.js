7cd3b4752724d0f6bae311917e684c0b060cdb65
Karen Gaspar <karenasgaspar@gmail.com>
2021-11-25 17:13:21 -0300

    Fiz função de trocar de página pra tela de detalhes no App.js

--- src/components/ServiçosNinjas/CardServiços.js
+++ src/components/ServiçosNinjas/CardServiços.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { converterData } from '../converterData';
+import styled from 'styled-components'
+
+const CardJobs = styled.div`
+background-color:#ACDAFF;
+display: flex;
+flex-direction: column;
+`
 
 export class CardServiços extends Component {
 
   render() {
       return(
-        <div>
+        <CardJobs>
           <h4>{this.props.job.title}</h4>
           <p><strong>Preço:</strong> R${this.props.job.price.toFixed(2)}</p>
           <p><strong>Prazo:</strong> {converterData(this.props.job.dueDate)}</p>
-          <button onClick={() => this.props.mudarTela('detalhes')}>Ver detalhes</button>
+          <button onClick={() => this.props.irPraTelaDetalhes(this.props.job.id)}>Ver detalhes</button>
           <button>Adicionar ao carrinho</button>
-        </div>
+        </CardJobs>
       )}
 }
--- src/components/ServiçosNinjas/ListaServiços.js
+++ src/components/ServiçosNinjas/ListaServiços.js
@@ -131,7 +131,7 @@ export class ListaServiços extends Component {
           }
         })
         .map((job) => {
-          return <CardServiços key={job.id} job={job}/>
+          return <CardServiços key={job.id} job={job} />
         })
       }
         </CardsContainer>
