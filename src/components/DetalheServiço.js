5df6ff5ba8036c6186c232f16e62ca2aed777a2a
Ruanar <ruana.piber@gmail.com>
2021-11-25 18:29:39 -0300

    comitando pra poder fazer o pull

--- src/components/DetalheServiço.js
+++ src/components/DetalheServiço.js
@@ -7,46 +7,51 @@ export class DetalheServiço extends Component {
     job: {},
   }
 
-  // componentDidMount(){
-  //   this.getJobById()
-  // }
+  componentDidMount(){
+    this.getJobById()
+  }
 
 
 
-  // getJobById = () => {
-  //   const id = this.props.id
-  //   const url = `https://labeninjas.herokuapp.com/jobs/${id}`
-  //   axios.get(url, {
-  //     headers: {
-  //       Authorization: "9a8a7b0c-9eba-4b46-9e82-f1ea4bf6f468"
-  //     }
-  //   })
-  //   .then((res) => {
-  //     console.log(res)
-  //     this.setState({job: res.data})
-  //   })
-  //   .catch((err) => {
-  //     alert(`Infelizmente, tivemos problemas ao processar sua solicitação: \n${err.response.data.message} ` )
-  //   })
-  // }
+  getJobById = () => {
+    const id = this.props.id
+    const url = `https://labeninjas.herokuapp.com/jobs/${id}`
+    axios.get(url, {
+      headers: {
+        Authorization: "9a8a7b0c-9eba-4b46-9e82-f1ea4bf6f468"
+      }
+    })
+    .then((res) => {
+      console.log(res)
+      this.setState({job: res.data})
+    })
+    .catch((err) => {
+      alert(`Infelizmente, tivemos problemas ao processar sua solicitação: \n${err.response.data.message} ` )
+    })
+  }
 
   
 
-  // render() {
-  //   const pagamento = this.state.job.paymentMethods && this.state.job.paymentMethods.map((pay) => {
-  //     return <li key={pay}>{pay}</li>
-  // })
-  //   return (
-  //     <div key= {job.id} >
-  //       <h2>{this.props.job.title}</h2>       
-  //       <p>{this.props.job.description}</p>
-  //       <p>Valor: R${this.props.job.price.toFixed(2)}</p>
-  //       <p>Prazo: {converterData(this.props.job.dueDate)}</p>
-  //       <p>formas de Pagamento Aceitas:</p>
-  //       {pagamento}
-  //       <button>Voltar pra lista</button>
-  //       <button>Adicionar no carrinho</button>
-  //     </div>
-  //   )
-  // }
+  render() {
+    const jobAtual = this.state.job.map((item) => {
+      return(
+        <div key= {item.id} >
+          <h2>{this.props.item.title}</h2>       
+          <p>{this.props.item.description}</p>
+          <p>Valor: R${this.props.item.price.toFixed(2)}</p>
+          <p>Prazo: {converterData(this.props.item.dueDate)}</p>
+          <p>formas de Pagamento Aceitas:</p>
+          {pagamento}
+          <button>Voltar pra lista</button>
+          <button>Adicionar no carrinho</button>
+        </div>
+      )
+    })
+    const pagamento = this.state.job.paymentMethods && this.state.job.paymentMethods.map((pay) => {
+      return <li key={pay}>{pay}</li>
+  })
+    return (
+      {jobAtual}
+    )
+  }
 }
