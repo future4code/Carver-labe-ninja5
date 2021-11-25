16b7d6354b59516988096c8f9b3df3a015a82a73
Ruanar <ruana.piber@gmail.com>
2021-11-25 13:32:00 -0300

    comitando pra poder fazer o pull

--- src/components/DetalheServiço.js
+++ src/components/DetalheServiço.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 export class DetalheServiço extends Component {
+  state = {
+    job: {},
+    id: ""
+  }
+
+  getJobById = ({id}) => {
+    const url = `https://labeninjas.herokuapp.com/jobs/${id}`
+    axios.get(url, {
+      headers: {
+        Authorization: "9a8a7b0c-9eba-4b46-9e82-f1ea4bf6f468"
+      }
+    })
+    .then((res) => {
+      console.log(res)
+      this.setState({job: res.data})
+    })
+    .catch((err) => {
+      alert(`Infelizmente, tivemos problemas ao processar sua solicitação: \n${err.response.data.message} ` )
+    })
+  }
 
   
 
