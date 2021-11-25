8aaf7d66ba1f5511494aa51e11edd58934c0b45f
Camila-Ferreira <caliopeferrer@gmail.com>
2021-11-25 16:50:29 -0300

    enviando pr para visualizar o conjunto todo. arrumei um pouco do layout, coloquei a funcionalidade de alguns botões, falta renderizar o carrinho vazio e os itens dentro dele

--- src/components/Carrinho/ItemDoCarrinho.js
+++ src/components/Carrinho/ItemDoCarrinho.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+const CardItemCarrinho = styled.div`
+border-radius: 5px;
+box-shadow: 1px 2px 1px 2px rgb(0 0 0 / 20%);
+margin: 5px;
+padding: 5px;
+text-align: center;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+background-color: #ACDAFF;
+width: 400px;
+
+button{
+width: fit-content;
+margin: 5px;
+padding: 5px;
+}
+
+`
+
 
 export class ItemDoCarrinho extends Component {
   render() {
     return (
-      <div>
-        <h3>Card com o serviço contratado</h3> 
-        <p>Título</p>
-        <p>Preço</p>
-        <button>Remover</button>
-      </div>
+      <CardItemCarrinho>
+        <h3></h3> 
+        <p>Quantidade: {this.props.quantidade}</p>
+        <p>Item: {this.props.nome}</p>
+        <p>Preço: {this.props.valor}</p>
+        <button onClick={() => this.props.removerCarrinho}>Remover</button>
+      </CardItemCarrinho>
     )
   }
 }
-
-{/* Para trocar a tela pelos botões de seu componente use a funcao onClick={() => this.props.mudarTela('nome da tela')}*/}
