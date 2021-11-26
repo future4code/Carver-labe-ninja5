707824f4704ff3a105139d5ef49b9040da13d4d8
Camila-Ferreira <caliopeferrer@gmail.com>
2021-11-26 12:36:04 -0300

    mexi numas coisas e acabei salvando o arquivo rs

--- src/components/Carrinho/ItemDoCarrinho.js
+++ src/components/Carrinho/ItemDoCarrinho.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import {carrinho} from './Carrinho'
 
 const CardItemCarrinho = styled.div`
 border-radius: 5px;
@@ -31,6 +32,9 @@ button:hover{
 box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
 }
 `
+// removerCarrinho = (id) => {
+// const remCarrinho = this.state.carrinho.filter(())
+// }
 
 
 export class ItemDoCarrinho extends Component {
